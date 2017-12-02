ddabfd8dae3063a596e2ac7174dab57132c8d02c
Kaustubh Mundra <kaustubh.mundra3@gmail.com>
2017-12-02 14:11:23 +0530

    iinit
    init

---
+++ dd.py
@@ -0,0 +1,30 @@
+import os 
+import sys
+
+# import dlib
+# import imutils
+import argparse
+
+import time
+# import cv2
+#import playsound
+
+from scipy.spatial import distance
+from imutils import face_utils
+
+
+from threading import Thread
+from imutils.video import VideoStream
+
+import numpy as np
+import pandas as pd
+
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+import cv2
+import pygame
+
+
+pygame.init()
